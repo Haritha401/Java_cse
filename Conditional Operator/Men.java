08adfc84baeb8c818abbbf5d0f223017d2cd580d
Haritha401 <harithabharitha48@gmail.com>
2025-03-13 10:53:01 +0530

    :hellllo

---
+++ Conditional Operator/Men.java
@@ -0,0 +1,12 @@
+public class Men{
+    public static void main(String args []){
+        if(10<2){
+            System.out.println(true);
+
+        }
+        if(10>2)
+            System.out.println(false);
+
+        }
+    }
+
