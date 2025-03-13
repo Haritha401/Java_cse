ed648ec5672955be3b394c722ae789df9a52af61
Haritha401 <harithabharitha48@gmail.com>
2025-03-13 11:16:32 +0530

    :this is my first voter list

---
+++ Major/Age.java
@@ -0,0 +1,10 @@
+public class Age{
+    public static void main(String args[]){
+        int x=35;
+        if(x>=18){
+        System.out.println("Major");
+        }
+        else
+        System.out.println("Minor");
+    }
+}
