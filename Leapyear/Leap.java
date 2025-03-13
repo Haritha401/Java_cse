674529c93e82d08c7f27a38f02bcdc225280f7f7
Haritha401 <harithabharitha48@gmail.com>
2025-03-13 14:46:03 +0530

    :this is the leapyear

---
+++ Leapyear/Leap.java
@@ -0,0 +1,14 @@
+public class Leap{
+    public static void main(String args[]){
+        int i=2020;
+        if(i%4==0){
+            System.out.println("leap year");
+        }
+        else{
+            System.out.println("Not leap year");
+    
+        }    
+    
+
+    }
+}
