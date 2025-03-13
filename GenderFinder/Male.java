f58f912543dc386ffee0fb1a673d860005571b9b
Haritha401 <harithabharitha48@gmail.com>
2025-03-13 11:33:36 +0530

    ;its female

---
+++ GenderFinder/Male.java
@@ -0,0 +1,12 @@
+public class Male{
+    public static void main(String args []){
+        char Gender='m';
+        if(Gender=='m'){
+        System.out.println("Female");
+        }
+        else{
+        System.out.println("Male");
+        }
+        }
+    }
+
