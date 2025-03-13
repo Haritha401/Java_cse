93c931dbd0a1403c7f7d5f9ec7802770c20c616e
Haritha401 <harithabharitha48@gmail.com>
2025-03-13 11:23:18 +0530

    :find postive negative

---
+++ PositiveNegative/Mem.java
@@ -0,0 +1,12 @@
+public class Mem{
+    public static void main(String args[]){
+    int x =35;
+    if(x>0){
+    System.out.println("Positivbe");
+    }
+    else
+    System.out.println("Negative");
+
+    }
+    
+}
