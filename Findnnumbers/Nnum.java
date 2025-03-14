3f0b41799172380336070f0507aab878a8c28f86
Haritha401 <harithabharitha48@gmail.com>
2025-03-14 11:00:28 +0530

    :find my nbrs

---
+++ Findnnumbers/Nnum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Nnum{
+    public static void main(String args[]){
+        Scanner Nnum=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=Nnum.nextInt();
+        int i=1;
+        while (i<=num){
+            System.out.println(i);
+            i=i+1;
+        }
+            }
+}
