0cc7b9e000692d666aada38c65a95f39aa547cec
Haritha401 <harithabharitha48@gmail.com>
2025-03-14 11:51:28 +0530

    :split the words

---
+++ Split/Sum.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        String name = "hello ,world";
+        String names[] = name.split(",");
+        System.out.println(names[1]);
+    }
+}        
