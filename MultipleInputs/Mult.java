e9f11a9b4e3d2f4702c556a259af357b9d8856d6
Haritha401 <harithabharitha48@gmail.com>
2025-03-14 14:56:56 +0530

    :this is the multipleinputs

---
+++ MultipleInputs/Mult.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Mult{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the n values:");
+        int n = input.nextInt();
+        int a[] = new int[n];
+        int i=0;
+        int j=0;
+        while(i<n){
+            System.out.println("Enter the value");
+            a[i] = input.nextInt();
+            i = i+1;
+        }
+        while(j<n){
+            System.out.println("Entered the value:"+a[j]);
+            j = j+1;
+        }
+    }
+}
