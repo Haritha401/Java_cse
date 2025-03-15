d43fdadfae4a757c49fbfdc6e79d934c78d38a79
Haritha401 <harithabharitha48@gmail.com>
2025-03-15 10:26:57 +0530

    : this is negative

---
+++ If else statement/Mean.java
@@ -0,0 +1,12 @@
+public class Mean{
+    public static void main(String args []){
+        int num =0;
+        if(num>=-1){
+            System.out.println("positive");
+        }
+        else if(num==0){
+            System.out.println("Neutral");
+        }
+        else
+        System.out.println("Negative");
+}   }
