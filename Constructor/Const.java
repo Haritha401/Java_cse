da5b0de88f58393e3fec0dd6380c2ed075e71dfb
Haritha401 <harithabharitha48@gmail.com>
2025-03-15 10:49:45 +0530

    : this is constructor

---
+++ Constructor/Const.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Const{
+    Const(){
+        System.out.println("abcd");
+    }
+    public static void main(String args[]){
+        Const obj = new Const();
+    }
+
+    }
